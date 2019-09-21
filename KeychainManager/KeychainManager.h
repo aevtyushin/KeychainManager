f154045709248f8935cf65b4128892d61d692b10
Евтюшин Андрей <evtyushin@corp.mail.ru>
2019-09-21 18:38:51 +0300

    init

---
+++ KeychainManager/KeychainManager.h
@@ -0,0 +1,19 @@
+//
+//  KeychainManager.h
+//  KeychainManager
+//
+//  Created by Андрей Евтюшин on 21/09/2019.
+//  Copyright © 2019 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeychainManager.
+FOUNDATION_EXPORT double KeychainManagerVersionNumber;
+
+//! Project version string for KeychainManager.
+FOUNDATION_EXPORT const unsigned char KeychainManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainManager/PublicHeader.h>
+
+
